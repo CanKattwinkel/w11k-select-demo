1360b11306b8ebe168d3c8ae8112d1907e786a02
Can Kattw <kattwinkel@w11k.de>
2017-05-09 17:39:12 +0200

    improve bundling using rollup

--- src/vendor.ts
+++ src/vendor.ts
@@ -6,4 +6,4 @@ import 'ng-metadata/common';
 /* w11k-select */
 import 'jquery/dist/jquery.js';
 import 'w11k-dropdownToggle/src/w11k-dropdownToggle.js';
-import 'w11k-select/build/index.js';
+import 'w11k-select/release/w11k-select.js';
